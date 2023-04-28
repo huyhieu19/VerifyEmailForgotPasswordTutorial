64086f394d1da4776fa7d132b5edb5cf5df9a73f
HuyHieu <huyhieu.hust2855@gmail.com>
2023-04-28 23:14:05 +0700

    Migration + Add Database

---
+++ VerifyEmailForgotPasswordTutorial/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using VerifyEmailForgotPasswordTutorial.Models;
+
+namespace VerifyEmailForgotPasswordTutorial.Data
+{
+    public class DataContext : DbContext
+    {
+        public IConfiguration Configuration { get; }
+
+
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options) 
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
--- VerifyEmailForgotPasswordTutorial/Program.cs
+++ VerifyEmailForgotPasswordTutorial/Program.cs
@@ -1,3 +1,6 @@
+global using Microsoft.EntityFrameworkCore;
+using VerifyEmailForgotPasswordTutorial.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Database
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
 
 var app = builder.Build();
 
