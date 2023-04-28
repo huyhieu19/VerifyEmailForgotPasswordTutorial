15ebcac3908876a07528d648ea2b7c50873ed197
HuyHieu <huyhieu.hust2855@gmail.com>
2023-04-28 23:34:04 +0700

    Create register method

---
+++ VerifyEmailForgotPasswordTutorial/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using VerifyEmailForgotPasswordTutorial.Data;
+using VerifyEmailForgotPasswordTutorial.Models;
+
+namespace VerifyEmailForgotPasswordTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public UserController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register(UserRegisterRequest request)
+        {
+            if (_context.Users.Any(p => p.Email == request.Email)) {
+                return BadRequest("User already exits");
+            }
+
+            CreatePasswordHash(request.Password,
+                    out byte[] passwordHash,
+                    out byte[] passwordSalt);
+
+            var user = new User
+            {
+                Email = request.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                VerificationToken = CreateRandomToken()
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User successfully created!");
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac
+                    .ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+    }
+
+}
